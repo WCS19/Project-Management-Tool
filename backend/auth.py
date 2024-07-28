82f68f52faf616351501636a9e9050475c255e4d
WCS19 <wcs@bu.edu>
2024-07-28 00:24:43 -0400

    removing commented code

--- backend/auth.py
+++ backend/auth.py
@@ -28,30 +28,3 @@ def register_auth_routes(app):
             return jsonify({'access_token': access_token})
         else:
             return jsonify({'message': 'Invalid credentials'}), 401
-
-
-
-# from flask import request, jsonify
-# from flask_jwt_extended import create_access_token
-# from backend.init import db, bcrypt
-# from backend.models import User
-
-# def register_auth_routes(app):
-#     @app.route('/register', methods=['POST'])
-#     def register():
-#         data = request.get_json()
-#         hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-#         new_user = User(username=data['username'], email=data['email'], password=hashed_password)
-#         db.session.add(new_user)
-#         db.session.commit()
-#         return jsonify({'message': 'User registered successfully'})
-
-#     @app.route('/login', methods=['POST'])
-#     def login():
-#         data = request.get_json()
-#         user = User.query.filter_by(username=data['username']).first()
-#         if user and bcrypt.check_password_hash(user.password, data['password']):
-#             access_token = create_access_token(identity={'id': user.id, 'username': user.username, 'email': user.email})
-#             return jsonify({'access_token': access_token})
-#         else:
-#             return jsonify({'message': 'Invalid credentials'}), 401
